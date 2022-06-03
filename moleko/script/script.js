53349fad28b4c4f2f7c5440ff1023a56df9c56d9
tiisetso40 <106803399+tiisetso40@users.noreply.github.com>
2022-06-03 10:15:03 +0200

    Add files via upload

---
+++ moleko/script/script.js
@@ -0,0 +1,12 @@
+var name = "Tiisetso";
+
+var surname = "Moleko";
+
+var age = "27";
+
+
+document.getElementById("description").innerHTML = "My name is " + name + 
+surname + "," +
+ age 
++ 
+"years old I was raised and born in Kraaipan village near Mahikeng in North West province.I have relocated to Johannesburg in 2018 to complete my studies, I would describe myself as a self driven, motivated person.I am energetic and a great communicator.i am punctual,dependable and can be counted upon to finish what I start.I consider myself hardworking, helpful, relable, organized, outgoing, honest and dependable.";
